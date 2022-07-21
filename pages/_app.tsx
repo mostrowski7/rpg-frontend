1ae7dc21a88b04951419ffe618ee41ebe0c58518
Michał Ostrowski <michal.ostrowski@imoli.pl>
2022-07-21 19:37:23 +0200

    feat: add initial project template

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+
+function MyApp() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default MyApp;
