4abd90af98ed7bc919a9f64ab7832b6c4cb64b91
Michał Ostrowski <michal.ostrowski@imoli.pl>
2022-07-25 18:57:57 +0200

    feat: add listen for mouse events in canvas

--- components/canvas/canvas.component.tsx
+++ components/canvas/canvas.component.tsx
@@ -1,9 +1,45 @@
+import { MouseEvent, useState } from "react";
 import styles from "./canvas.styles";
 
 const Canvas = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const [mouseLeave, setMouseLeave] = useState(false);
+  const [mouseDown, setMouseDown] = useState(false);
+
+  const onMouseMove = (e: MouseEvent) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setCoords({ x, y });
+  };
+
+  const onMouseLeave = () => {
+    setMouseLeave(true);
+  };
+
+  const onMouseEnter = () => {
+    setMouseLeave(false);
+  };
+
+  const onMouseDown = () => {
+    setMouseDown(true);
+  };
+
+  const onMouseUp = () => {
+    setMouseDown(false);
+  };
+
   return (
     <div className={styles.container}>
-      <canvas width="1920" height="1080"></canvas>
+      <canvas
+        width="1920"
+        height="1080"
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseLeave}
+        onMouseEnter={onMouseEnter}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+      ></canvas>
     </div>
   );
 };
