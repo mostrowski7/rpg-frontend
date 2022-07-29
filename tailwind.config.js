2dbdc16952b5826a8f8a6135bdbcb2271b1e6ff0
Michał Ostrowski <michal.ostrowski@imoli.pl>
2022-07-29 18:25:11 +0200

    feat: add canvas background color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        canvasBackground: "#f2f2f2"
+      }
+    },
   },
   plugins: [],
 };
